85c9616fe77d7066776eaecd13d98825f1044705
David Igou <igou.david@gmail.com>
2019-12-11 20:51:15 -0500

    Add template folder syncing

--- main.tf
+++ main.tf
@@ -191,7 +191,51 @@ resource "aws_instance" "k3os_master" {
   associate_public_ip_address = true # Instances have public, dynamic IP
   vpc_security_group_ids      = ["${aws_security_group.k3os_sg.id}", "${aws_security_group.k3os_api.id}"]
   key_name                    = "${var.keypair_name}"
-  user_data                   = "${templatefile("${path.module}/files/config_server.sh", { ssh_keys = var.ssh_keys, data_sources = var.data_sources, kernel_modules = var.kernel_modules, sysctls = var.sysctls, dns_nameservers = var.dns_nameservers, ntp_servers = var.ntp_servers, k3s_cluster_secret = var.k3s_cluster_secret })}"
+  user_data                   = "${templatefile("${path.module}/files/config_server.sh", { ssh_keys = var.ssh_keys, data_sources = var.data_sources, kernel_modules = var.kernel_modules, sysctls = var.sysctls, dns_nameservers = var.dns_nameservers, ntp_servers = var.ntp_servers, k3s_cluster_secret = var.k3s_cluster_secret, k3s_args = var.k3s_args })}"
+
+
+  provisioner "file" {
+    source      = "${path.module}/manifests"
+    destination = "/home/rancher/"
+    connection {
+      type  = "ssh"
+      host  = "${aws_instance.k3os_master.public_dns}"
+      agent = true
+      user  = "rancher"
+    }
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 20",
+      "sudo mv /home/rancher/manifests/* /var/lib/rancher/k3s/server/manifests/",
+      "sudo chown -R root:root /var/lib/rancher/k3s/server/manifests/",
+      "sudo chmod -R 0600 /var/lib/rancher/k3s/server/manifests/",
+      "sudo cp /etc/rancher/k3s/k3s.yaml /home/rancher/",
+      "sudo chown rancher:rancher /home/rancher/k3s.yaml"
+    ]
+    connection {
+      type        = "ssh"
+      host        = "${aws_instance.k3os_master.public_dns}"
+      agent       = true
+      user        = "rancher"
+      script_path = "/home/rancher/terraform.sh"
+    }
+  }
+  provisioner "local-exec" {
+    command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null rancher@${aws_instance.k3os_master.public_dns}:~/k3s.yaml kubeconfig/k3s.yaml"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "rm /home/rancher/k3s.yaml"
+    ]
+    connection {
+      type        = "ssh"
+      host        = "${aws_instance.k3os_master.public_dns}"
+      agent       = true
+      user        = "rancher"
+      script_path = "/home/rancher/terraform.sh"
+    }
+  }
   tags = {
     Name                            = "k3os_master",
     "kubernetes.io/cluster/default" = "owned"
